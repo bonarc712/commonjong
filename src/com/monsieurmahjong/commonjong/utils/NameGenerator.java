81a976975f22d595a4eb2ac4fb46615299096b60
Loic Roberge <loic-alexandre.roberge@dental-wings.com>
2020-09-27 00:59:46 -0400

    Created ant build to check that the project compiles at every commit

--- src/com/monsieurmahjong/commonjong/utils/NameGenerator.java
+++ src/com/monsieurmahjong/commonjong/utils/NameGenerator.java
@@ -1,34 +1,32 @@
 package com.monsieurmahjong.commonjong.utils;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class NameGenerator
 {
-	public static String generateName()
-	{
-		return generateNameOtherThan(Collections.EMPTY_LIST);
-	}
+    public static String generateName()
+    {
+        return generateNameOtherThan(new ArrayList<>());
+    }
 
-	public static String generateNameOtherThan(List<String> names)
-	{
-		List<String> possibleNames = getPossibleNames();
-		possibleNames.removeAll(names);
-		return possibleNames.get((int) (Math.random() * possibleNames.size()));
-	}
+    public static String generateNameOtherThan(List<String> names)
+    {
+        List<String> possibleNames = getPossibleNames();
+        possibleNames.removeAll(names);
+        return possibleNames.get((int) (Math.random() * possibleNames.size()));
+    }
 
-	private static List<String> getPossibleNames()
-	{
-		List<String> possibleNames = new ArrayList<>();
-		possibleNames.add("A");
-		possibleNames.add("B");
-		possibleNames.add("C");
-		possibleNames.add("D");
-		possibleNames.add("E");
-		possibleNames.add("F");
-		possibleNames.add("G");
-		possibleNames.add("H");
-		return possibleNames;
-	}
+    private static List<String> getPossibleNames()
+    {
+        List<String> possibleNames = new ArrayList<>();
+        possibleNames.add("A");
+        possibleNames.add("B");
+        possibleNames.add("C");
+        possibleNames.add("D");
+        possibleNames.add("E");
+        possibleNames.add("F");
+        possibleNames.add("G");
+        possibleNames.add("H");
+        return possibleNames;
+    }
 }
