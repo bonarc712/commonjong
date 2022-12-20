7e24f2af0c0a35cbfab4c73944c3340bebec657c
Loïc Roberge <lorob64@gmail.com>
2022-12-19 23:33:21 -0500

    Bring back direct call to toList()

--- src/main/java/com/monsieurmahjong/commonjong/rules/riichi/scoring/RiichiScoring.java
+++ src/main/java/com/monsieurmahjong/commonjong/rules/riichi/scoring/RiichiScoring.java
@@ -1,7 +1,6 @@
 package com.monsieurmahjong.commonjong.rules.riichi.scoring;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.monsieurmahjong.commonjong.game.Hand;
 import com.monsieurmahjong.commonjong.rules.generic.Scoring;
@@ -31,6 +30,6 @@ public class RiichiScoring implements Scoring
     public List<Yaku> getValidYakus(Hand hand, List<TileGroup> tileGroups, RiichiScoringParameters parameters)
     {
         var yakus = Yakus.getStandardYakus(hand, tileGroups, parameters);
-        return yakus.stream().filter(Yaku::isValid).collect(Collectors.toList());
+        return yakus.stream().filter(Yaku::isValid).toList();
     }
 }
